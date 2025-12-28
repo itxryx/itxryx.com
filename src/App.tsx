1a2a5fa823108e67d7cd6240503d145a00aa2c4f
komatsu ryo <electrooculogram@gmail.com>
2025-12-29 02:11:04 +0900

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import { Layout } from './components/Layout'
 import { HomePage } from './pages/HomePage'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   )
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+export function NotFoundPage() {
+  return (
+    <>
+      <div className="my-[4rem] flex flex-col items-center justify-center">
+        <h2>404 - Page Not Found</h2>
+        <p className="mt-[2.4rem]">お探しのページは見つかりませんでした。</p>
+      </div>
+    </>
+  )
+}
